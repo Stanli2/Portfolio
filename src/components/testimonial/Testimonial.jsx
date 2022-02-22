239a5576d1f06e5d1d287129b97029d81fe6b3db
Sley23 <obiwonike@gmail.com>
2022-02-22 17:21:41 +0100

    Fourth commit

--- src/components/testimonial/Testimonial.jsx
+++ src/components/testimonial/Testimonial.jsx
@@ -4,7 +4,9 @@ import "./testimonial.scss"
 export default function Testimonial() {
   return (
     <div className="testimonial" id="testimonial">
-      
+      <div className="heading">
+        <h1 className="heading">My Skills</h1>
+      </div>
     </div>
   )
 }
